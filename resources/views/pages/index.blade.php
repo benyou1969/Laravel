6812020f64f16d7707e3ed75bd1542cf81dbb1d7
BenYou <ismail.mca191@gmail.com>
2018-05-29 03:20:02 +0100

    Basic Routing & Controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{config('app.locale')}}">
+  <head>
+    <meta charset="utf-8">
+    <title>{{config('app.name','BenYou')}}</title>
+  </head>
+  <body>
+    <h2>Index</h2>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+//All Information About Routing visit this link {https://laravel.com/docs/5.6/routing}
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+     return view('welcome');
+    // return ('<h2 style="text-align:center">Welcome</h2>');
 });
+Route::get('/','PagesController@index');
+Route::get('/services','PagesController@services');
+// Available Router Methods
+/*
+Route::get($uri, $callback);
+Route::post($uri, $callback);
+Route::put($uri, $callback);
+Route::patch($uri, $callback);
+Route::delete($uri, $callback);
+Route::options($uri, $callback);
+*/
+//Basic Routing
+Route::get('foo', function () {
+    return 'Hello World';
+});
+Route::any('foo', function () {
+  return 'Hello World';
+});
+Route::view('/about', 'about');
+// Required Parameters
+Route::get('/users/{id}', function ($id) {
+    return 'This Is user '. $id;
+});
+// Required Parameters
+Route::get('user/{name?}',function($name){
+  return 'My name is : ' .$name;
+});
+// Regular Expression Constraints
+Route::get('user-id/{id}', function ($id) {
+    return 'user id: ' . $id ;
+})->where('id', '[0-9]+');
+Route::get('user-name/{name}', function ($name) {
+    return 'user name  is : '. $name;
+})->where('name', '[A-Za-z]+');
+Route::get('user/{id}/{name}',function($id,$name){
+  return 'username is: ' . $name . ', and id : ' . $id;
+})->where(['id'=> '[0-9]+','name'=>'[a-z]+']);
+// Global Constraints
+// Named Routes
+// Route::get('user/profile', 'UserController@showProfile')->name('profile');
